fb06676f70a012574dc0d5f5f84e31e3712268ce
Thiago Saraiva <thiago.saraiva@kwiff.com>
2018-07-19 10:41:50 +0100

    implementing the database

---
+++ app/src/main/java/com/examples/thsaraiva/roomwordsamplekt/data/database.kt
@@ -0,0 +1,30 @@
+package com.examples.thsaraiva.roomwordsamplekt.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.examples.thsaraiva.roomwordsamplekt.data.dao.WordDao
+
+@Database(entities = [(Word::class)], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        private const val DB_NAME = "wordRoom.db"
+        private lateinit var INSTANCE: WordRoomDatabase
+
+        fun getInstance(ctx: Context): WordRoomDatabase {
+            if (INSTANCE == null) {
+                synchronized(WordRoomDatabase::class) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(ctx.applicationContext,
+                                WordRoomDatabase::class.java,
+                                WordRoomDatabase.DB_NAME).build()
+                    }
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
