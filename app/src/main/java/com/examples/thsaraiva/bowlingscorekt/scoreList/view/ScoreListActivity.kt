e99b72fe06734c56e276e5a3aa3bf2073a826e57
Thiago Saraiva <thiago.saraiva@kwiff.com>
2018-07-29 23:22:35 +0100

    adding delete all feature to the app

--- app/src/main/java/com/examples/thsaraiva/bowlingscorekt/scoreList/view/ScoreListActivity.kt
+++ app/src/main/java/com/examples/thsaraiva/bowlingscorekt/scoreList/view/ScoreListActivity.kt
@@ -71,11 +71,11 @@ class ScoreListActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.clear_all -> {
+                scoreViewModel.deleteAll()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
